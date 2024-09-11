ae0fb3847b7e00c8b952240021f5c1f96b60f402
Low-go <lorranalvesgaldino98@gmail.com>
2024-09-10 16:01:40 -1000

    Remove dev containers for now. configure settings to connect with database, make migrations and tables for objects

--- back-end/company_network/settings.py
+++ back-end/company_network/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_path = BASE_DIR / '.env'
+
+load_dotenv(dotenv_path=dotenv_path)
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,6 +81,8 @@ WSGI_APPLICATION = 'company_network.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+
 
 DATABASES = {
     'default': {

6404af40a0d354ab6cf6fa94f51b9db31712d755
Low-go <lorranalvesgaldino98@gmail.com>
2024-12-17 22:38:50 -1000

    change base styles for index.css. Add some new folders and file structures for future organization

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,33 +1,9 @@
-import './App.css';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Toolbar from '@mui/material/Toolbar';
-import Sidebar from './Components/Sidebar';
-import Navbar from './Components/Navbar'; 
-import { SIDEBAR_WIDTH } from './constants';
+
+
 
 function App() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Sidebar />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          bgcolor: 'background.default',
-          width: { sm: `calc(100% - ${SIDEBAR_WIDTH}px)` }
-        }}
-      >
-        <Navbar /> 
-        <Toolbar /> 
-        <Box sx={{ p: 3 }}>
-          <Typography>
-            This is where main content goes
-          </Typography>
-        </Box>
-      </Box>
-    </Box>
+    <div className="app"></div>
   );
 }
 

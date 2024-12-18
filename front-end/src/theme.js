8f245494a49a86ccdaea1a8818810041c2c33365
Low-go <lorranalvesgaldino98@gmail.com>
2024-12-18 11:53:13 -1000

    function to return theme and colormode and allow switching of all colors

--- front-end/src/theme.js
+++ front-end/src/theme.js
@@ -3,6 +3,7 @@
 import { createContext, useState, useMemo} from "react";
 import { createTheme } from "@mui/material/styles";
 
+
 // color design tokens
 export const tokens = (mode) => ({
    ...(mode === 'dark'
@@ -138,3 +139,97 @@ export const tokens = (mode) => ({
 //     800: "#66002d",
 //     900: "#330017"
 // },
+
+
+// mui theme settings. Just returns tokens according to theme 
+export const themeSettings = (mode) =>{
+    const colors = tokens(mode);
+
+    return {
+        palette: {
+            ...(mode === 'dark'
+                ?{
+                    primary: {
+                        main: colors.primary[500],
+                    },
+                    secondary: {
+                        main: colors.greenAccent[500],
+                    },
+                    neutral: {
+                        dark: colors.grey[700],
+                        main: colors.grey[500],
+                        light: colors.grey[100]
+                    },
+                    background: {
+                        default: colors.primary[500],
+                    } 
+                } : {
+                    primary: {
+                        main: colors.primary[100],
+                    },
+                    secondary: {
+                        main: colors.greenAccent[500],
+                    },
+                    neutral: {
+                        dark: colors.grey[700],
+                        main: colors.grey[500],
+                        light: colors.grey[100]
+                    },
+                    background: {
+                        default: "#fcfcfc",
+                    } 
+                }
+            )
+        },
+        typography: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 14,
+            },
+        }
+
+    };
+};
+
+// create react context for the color mode
+export const ColorModeContext = createContext({
+    toggleColorMode: () => {}
+});
+
+export const useMode = () => {
+    const [mode, setMode] = useState("dark");
+
+    const colorMode = useMemo(
+        () => ({
+        toggleColorMode: () =>
+            setMode((prev) => (prev === "light" ? "dark" : "light")),
+        }),
+        []
+    );
+
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+    return [theme, colorMode];
+}

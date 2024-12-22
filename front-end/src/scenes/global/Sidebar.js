f1f1fe3cbf84082fe716cfcef3f4a2d53fafbad3
Low-go <lorranalvesgaldino98@gmail.com>
2024-12-22 13:21:05 -1000

    add routes to app, start work on sidebar

--- front-end/src/scenes/global/Sidebar.js
+++ front-end/src/scenes/global/Sidebar.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import  HomeOutlinedIcon  from "@mui/icons-material/HomeOutlined";
+import  FeedOutlinedIcon  from "@mui/icons-material/FeedOutlined";
+import  HubOutlinedIcon  from "@mui/icons-material/HubOutlined";
+import  QuizOutlinedIcon  from "@mui/icons-material/QuizOutlined";
+// maybe i should do a calander not sure yet
 
 const Sidebar = () => {
+
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false); // collapse sidebar or not
+  const [selected, setSelected] = useState("Dashboard"); // which page we are at
+
   return (
     <div>
       Sidebar

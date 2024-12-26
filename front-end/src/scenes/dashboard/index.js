61991657b785d9c0a74907b016fce970ace6272b
Low-go <lorranalvesgaldino98@gmail.com>
2024-12-26 13:33:54 -1000

    add two columns in dashboard for future node and info. Delete some older files. Need to adjust colros now

--- front-end/src/scenes/dashboard/index.js
+++ front-end/src/scenes/dashboard/index.js
@@ -1,13 +1,54 @@
-import Header from "../../Components/Header"
-import { Box } from "@mui/material"
+import Header from "../../Components/Header";
+import { Box, useTheme, Typography } from "@mui/material";
+import { tokens } from "../../theme";
 
 const Dashboard = () => {
-    return (
-      <Box m="20px" display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to Airflight-Enterprise"/>
+
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to Airflight-Enterprise" />
       </Box>
-    )
-  }
-  
-  export default Dashboard
-  
+
+      <Box 
+        display="flex" 
+        justifyContent="space-between" 
+        alignItems="stretch" 
+        height="65vh" // lets see if this height is ok
+        mt="20px"
+        gap="20px"
+        
+      >
+        {/**Left Box */}
+        <Box 
+          flex="1" // Equal with using flex 1 for both containers
+          bgcolor={colors.primary[400]}
+          p="20px"
+          borderRadius="8px"
+          overflow="scroll" // left box should probably have this
+        >
+          <Typography variant="h6" color={colors.grey[100]}>
+            Left Grid Content
+          </Typography>
+        </Box>
+
+         {/**Right Box */}
+        <Box 
+          flex="1" 
+          bgcolor={colors.primary[600]}
+          p="20px"
+          borderRadius="8px"
+        >
+          <Typography variant="h6" color={colors.grey[100]}>
+            Right Grid Content
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;

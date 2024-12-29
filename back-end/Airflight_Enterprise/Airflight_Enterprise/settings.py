3b0a9e333ceda1ede5af494357f80e6275a66e7a
Low-go <lorranalvesgaldino98@gmail.com>
2024-12-28 22:36:50 -1000

    basic api endpoints setup

--- back-end/Airflight_Enterprise/Airflight_Enterprise/settings.py
+++ back-end/Airflight_Enterprise/Airflight_Enterprise/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',    # just added this so I can create rest API
 ]
 
 MIDDLEWARE = [
--- back-end/Airflight_Enterprise/FamilyTree/views.py
+++ back-end/Airflight_Enterprise/FamilyTree/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+# import local data
+from .serializers import CompanySerializer, FleetSerializer, ContactSerializer, PartSerializer
+from .models import Company, Fleet, Contact, Part
+
+# ViewSets for API endpoint.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class FleetViewSet(viewsets.ModelViewSet):
+    queryset = Fleet.objects.all()
+    serializer_class = FleetSerializer
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+class PartViewSet(viewsets.ModelViewSet):
+    queryset = Part.objects.all()
+    serializer_class = PartSerializer
+

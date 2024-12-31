23335b4dd087656381b4816f796d95eaf09105fd
Low-go <lorranalvesgaldino98@gmail.com>
2024-12-30 22:58:51 -1000

    add notes field in models

--- back-end/Airflight_Enterprise/FamilyTree/models.py
+++ back-end/Airflight_Enterprise/FamilyTree/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Company(models.Model):
     company_name = models.CharField(max_length = 70)
     phone_number = models.CharField(max_length=19, blank=True) # optional phone number
+    notes = models.TextField(blank=True, null=True)
 
 
 class Contact(models.Model):
@@ -13,11 +14,13 @@ class Contact(models.Model):
     last_name = models.CharField(max_length=50)
     role = models.CharField(max_length=30)
     email = models.EmailField(max_length = 254)
+    notes = models.TextField(blank=True, null=True)
 
 class Fleet(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='fleets')
     model = models.CharField(max_length=50)
     fabrication_date = models.DateField(null=True, blank=True) # optional
+    notes = models.TextField(blank=True, null=True)
 
 # Most fields are optional. employer can try to get as much info as he 
 # wants but it is not required
@@ -29,3 +32,4 @@ class Part(models.Model):
     expected_lifespan = models.IntegerField(blank=True, null=True) # need to get back to this one and adjust it
     manufacturer = models.CharField(max_length=100, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)

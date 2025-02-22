c125aac3f06ab726512c6ec2b18c96ad2a79380b
Low-go <lorranalvesgaldino98@gmail.com>
2025-02-21 15:32:44 -1000

    add a special stylized node. Next need to create custom dark/light node colors section in themes

--- front-end/src/scenes/Right/RightScreen.js
+++ front-end/src/scenes/Right/RightScreen.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import { Box, useTheme, Typography } from "@mui/material";
 import { tokens } from '../../theme';
-import { ReactFlow, useNodesState, useEdgesState, addEdge } from '@xyflow/react';
+import { ReactFlow, useNodesState, useEdgesState, addEdge, MiniMap, Controls, Background } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 const RightScreen = () => {
@@ -11,21 +11,37 @@ const RightScreen = () => {
 
     // React flow initial test first flow
     const initialNodes = [
-        { id: '1', position: { x: 0, y: 0}, data: {label: '1'}},
-        { id: '2', position: { x: 0, y: 100}, data: {label: '2'}}
+            { 
+                id: '1', 
+                position: { x: 100, y: 100}, 
+                style: {
+                    background: 'linear-gradient(180deg, #1F2A40 0%, #141B2D 100%)',
+                    border: '2px solid #4CCEAC',
+                    padding: '15px',
+                    borderRadius: '8px',
+                    width: 200,
+                    fontSize: '16px',
+                    color: '#fff',
+                    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                },
+                data: { 
+                    label: 'Company Name'
+                }
+            }
+        // { id: '2', position: { x: 0, y: 100}, data: {label: '2'}}
     ];
 
-    const initialEdges = [ { id: 'e1-2', source: '1', target: '2'} ];
+    // const initialEdges = [ { id: 'e1-2', source: '1', target: '2'} ];
 
 
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    // const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
 
-    const onConnect = useCallback(
-        (params) => setEdges((eds) => addEdge(params, eds)),
-        [setEdges],
-    );
+    // const onConnect = useCallback(
+    //     (params) => setEdges((eds) => addEdge(params, eds)),
+    //     [setEdges],
+    // );
 
     return (
         <Box
@@ -34,16 +50,18 @@ const RightScreen = () => {
             p="20px"
             borderRadius="4px"
         >
-            {/* <Typography variant="h6" color={colors.grey[100]}>
-                Right Grid Content
-            </Typography> */}
 
             <ReactFlow 
                 nodes= {nodes} 
-                edges = {edges} 
+                // edges = {edges} 
                 onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}/>
+                // onEdgesChange={onEdgesChange}
+                // onConnect={onConnect}
+                >
+                <Controls/>
+                {/* <MiniMap/> */}
+                <Background variant="dots" gap={12} size={1} />
+            </ReactFlow>
         </Box>
     )
 }

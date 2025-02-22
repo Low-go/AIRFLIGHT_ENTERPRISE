f4ac529661d64260176ab33fcb0212d08e4a355c
Low-go <lorranalvesgaldino98@gmail.com>
2025-02-21 15:04:00 -1000

    initial use of react flow, playing around with nodes and edges on right screen

--- front-end/src/scenes/Right/RightScreen.js
+++ front-end/src/scenes/Right/RightScreen.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Box, useTheme, Typography } from "@mui/material";
 import { tokens } from '../../theme';
+import { ReactFlow, useNodesState, useEdgesState, addEdge } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
 
 const RightScreen = () => {
 
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
+
+    // React flow initial test first flow
+    const initialNodes = [
+        { id: '1', position: { x: 0, y: 0}, data: {label: '1'}},
+        { id: '2', position: { x: 0, y: 100}, data: {label: '2'}}
+    ];
+
+    const initialEdges = [ { id: 'e1-2', source: '1', target: '2'} ];
+
+
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+
+    const onConnect = useCallback(
+        (params) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
+    );
 
     return (
         <Box
@@ -14,9 +34,16 @@ const RightScreen = () => {
             p="20px"
             borderRadius="4px"
         >
-            <Typography variant="h6" color={colors.grey[100]}>
+            {/* <Typography variant="h6" color={colors.grey[100]}>
                 Right Grid Content
-            </Typography>
+            </Typography> */}
+
+            <ReactFlow 
+                nodes= {nodes} 
+                edges = {edges} 
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}/>
         </Box>
     )
 }

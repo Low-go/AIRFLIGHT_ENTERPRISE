5a516049b22192e09c80d055f924d8881fce4bd8
Low-go <lorranalvesgaldino98@gmail.com>
2025-03-27 17:29:46 -1000

    move nodebuttonClick into utils file, pass respective parameters

--- front-end/src/scenes/Right/RightScreen.js
+++ front-end/src/scenes/Right/RightScreen.js
@@ -8,6 +8,8 @@ import SmallNode from './Nodes/Small.Node';
 import MediumNode from './Nodes/MediumNode';
 import HubOutlinedIcon from "@mui/icons-material/HubOutlined";
 import { useCompany } from '../../contexts/CompanyContext';
+import { handleNodeButtonClick } from '../../utils/nodeGenerationUtils';
+
 
 const RightScreen = () => {
   const theme = useTheme();
@@ -15,143 +17,23 @@ const RightScreen = () => {
   const isDarkMode = theme.palette.mode === "dark";
   const { selectedCompany } = useCompany();
   
+  
   // Memoize nodeTypes, no rerenders allowed if nothings changed
   const nodeTypes = useMemo(() => ({
     bigNode: (props) => <BigNode 
       {...props} 
       isDarkMode={isDarkMode}
-      onHandleClick={(nodeId, handleId) => handleNodeButtonClick(nodeId, handleId)}
+      onHandleClick={(nodeId, handleId) => handleNodeButtonClick(
+        nodeId, 
+        handleId, 
+        setNodes, 
+        setEdges, 
+        isDarkMode
+      )}
       />,
     smallNode: (props) => <SmallNode {...props} isDarkMode={isDarkMode}/>,
     mediumNode: (props) => <MediumNode {...props} isDarkMode={isDarkMode}/>
   }), [isDarkMode]);
-
-
-  //---------this will be the magic for retraction, I might pull it out of this file later ----------------------------------------------------
-
-  // this will be my attempt at handling node retractions and creations
-  const handleNodeButtonClick = (nodeId, handleId) => {
-    setNodes((currentNodes) => {
-
-      // lets give this bad boy a shot ahhhhhhhh
-      
-      // instead of checking by array index number or id lets just assign an id or name. i don't know how practical this is
-      const existingContactsNodeIndex = currentNodes.findIndex(
-        node => node.data.name === 'Contacts'
-      );
-      const existingFleetsNodeIndex = currentNodes.findIndex(
-        node => node.data.name === 'Fleets'
-      );
-
-      const contactsNodeId = `${nodeId}-contacts`;
-      const fleetsNodeId = `${nodeId}-fleets`;
-  
-      // this is if we already have these existing nodes in out system
-      if (handleId === 'top-right-handle') {
-
-        //const existingNodeIndex = nodes.findIndex(node => node.id === contactsNodeId);
-        // If no contacts node exists, create one
-        if (existingContactsNodeIndex === -1) {
-          const contactsNodeId = `contacts-${Date.now()}`;
-          const contactsNode = {
-            id: contactsNodeId,
-            type: 'smallNode',
-            position: { 
-              x: currentNodes[0].position.x + 250, 
-              y: currentNodes[0].position.y - 150 
-            },
-            data: {
-              name: 'Contacts',
-            },
-          };
-  
-          // Create a new edge connecting the original node to the contacts node
-          const newEdge = {
-            id: `e${nodeId}-${contactsNodeId}`,
-            source: nodeId,
-            target: contactsNodeId,
-            sourceHandle: 'top-right-handle',
-            targetHandle: 'left-handle',
-            animated: true,
-            style: { 
-              stroke: isDarkMode ? "#ffffff" : "#000000", 
-              strokeWidth: 2
-            }
-          };
-  
-          // Update both nodes and edges
-          setEdges(edges => [...edges, newEdge]);
-          return [...currentNodes, contactsNode];
-        } 
-        // If contacts node exists, remove it
-        else {
-          // Remove edges
-          setEdges(edges => 
-            edges.filter(edge => 
-              edge.source !== currentNodes[existingContactsNodeIndex].id && 
-              edge.target !== currentNodes[existingContactsNodeIndex].id
-            )
-          );
-          return currentNodes.filter(node => node.data.name !== 'Contacts');
-        }
-      }
-      
-      // If bottom-right handle is clicked (for Fleets)
-      if (handleId === 'bottom-right-handle') {
-        // If no fleets node exists, create one
-        if (existingFleetsNodeIndex === -1) {
-         
-          const fleetsNode = {
-            id: fleetsNodeId,
-            type: 'smallNode',
-            position: { 
-              x: currentNodes[0].position.x + 250, 
-              y: currentNodes[0].position.y + 150 
-            },
-            data: {
-              name: 'Fleets',
-            },
-          };
-  
-          // Create a new edge connecting the original node to the fleets node
-          const newEdge = {
-            id: `e${nodeId}-${fleetsNodeId}`,
-            source: nodeId,
-            target: fleetsNodeId,
-            sourceHandle: 'bottom-right-handle',
-            targetHandle: 'left-handle',
-            animated: true,
-            style: { 
-              stroke: isDarkMode ? "#ffffff" : "#000000", 
-              strokeWidth: 2
-            }
-          };
-  
-          // Update both nodes and edges
-          setEdges(edges => [...edges, newEdge]);
-          return [...currentNodes, fleetsNode];
-        } 
-        // If fleets node exists, remove it
-        else {
-          // Remove edges
-          setEdges(edges => 
-            edges.filter(edge => 
-              edge.source !== currentNodes[existingFleetsNodeIndex].id && 
-              edge.target !== currentNodes[existingFleetsNodeIndex].id
-            )
-          );
-          return currentNodes.filter(node => node.data.name !== 'Fleets');
-        }
-      }
-      
-      // If no matching handle, return current nodes
-      return currentNodes;
-  })}
-
-
-
-  //------------End of code retraction stuff ----------------------------------------------------------
-
 
   
   // Initialize states with empty arrays

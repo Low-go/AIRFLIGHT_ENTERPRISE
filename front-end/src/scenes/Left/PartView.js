4dac7c78d39abb07dae9b785284b195fa1ee90df
Low-go <lorranalvesgaldino98@gmail.com>
2025-04-29 22:45:29 -1000

    begin PartView page initial base setup

---
+++ front-end/src/scenes/Left/PartView.js
@@ -0,0 +1,16 @@
+import { Box, Typography, Button, useTheme, Snackbar } from '@mui/material';
+import { tokens } from '../../theme';
+import React from 'react';
+
+
+const PartView = () => {
+
+    return (
+        <Box>
+            test
+        </Box>
+    )
+}
+
+
+export default PartView
